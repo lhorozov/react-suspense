062eee68b7615263c2908651d8d9c3f22758893e
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-03-01 21:34:08 +0200

    Simple Data-fetching-extra credit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,13 +2,20 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {PokemonDataView, fetchPokemon} from '../pokemon'
+import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
 
 let pokemon
+let error
 
-const pokemonPromise = fetchPokemon('pikachu').then(data => (pokemon = data))
+let pokemonPromise = fetchPokemon('pikacha').then(
+  data => (pokemon = data),
+  e => (error = e),
+)
 
 function PokemonInfo() {
+  if (error) {
+    throw error
+  }
   if (!pokemon) {
     throw pokemonPromise
   }
@@ -27,9 +34,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback="Loading...">
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonErrorBoundary>
+          <React.Suspense fallback="Loading...">
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
